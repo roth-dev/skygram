5b46c140941c6277ab43c1c925268e881ebe0f3f
roth-dev <roth.dev.ops@gmail.com>
2025-02-25 22:52:34 +0700

    refactor: simplify button component imports and structure

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,17 +2,14 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 import {
-  View,
   TouchableOpacity,
   TouchableOpacityProps,
   TextProps,
-  Text,
 } from "react-native";
 import { Slot } from "expo-router";
-import { ThemedText } from "../ThemedText";
-import { IconSymbol, IconSymbolName, IconSymbolProps } from "./IconSymbol";
+import { IconSymbol, IconSymbolProps } from "./IconSymbol";
+import { Text } from "./text";
 
-// inspired by shadcn's button component
 const buttonVariants = cva(
   "flex-row inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
