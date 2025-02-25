9a4b942f5ac39d8f13af8deb851254e6dda02883
roth-dev <roth.dev.ops@gmail.com>
2025-02-25 22:51:10 +0700

    custom view component

---
+++ src/components/ui/view.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View as BaseView } from "react-native";
+import { cn } from "@/lib/utils";
+
+const View = React.forwardRef<
+  React.ElementRef<typeof BaseView>,
+  React.ComponentPropsWithoutRef<typeof BaseView>
+>(({ className, ...props }, ref) => (
+  <BaseView
+    ref={ref}
+    className={cn("dark:bg-neutral-950 bg-white", className)}
+    {...props}
+  />
+));
+
+View.displayName = "View";
+
+export { View };
