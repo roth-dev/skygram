a1dcfe8909f509531d747ad10055679a77a4c99e
roth-dev <roth.dev.ops@gmail.com>
2025-02-26 16:37:45 +0700

    feat: add default preferences

---
+++ src/state/queries/prefs/const.ts
@@ -0,0 +1,43 @@
+import { DEFAULT_LOGGED_OUT_LABEL_PREFERENCES } from "./moderation";
+import { ThreadViewPreferences, UsePreferencesQueryResponse } from "./types";
+
+export const DEFAULT_HOME_FEED_PREFS: UsePreferencesQueryResponse["feedViewPrefs"] =
+  {
+    hideReplies: false,
+    hideRepliesByUnfollowed: true, // Legacy, ignored
+    hideRepliesByLikeCount: 0, // Legacy, ignored
+    hideReposts: false,
+    hideQuotePosts: false,
+    lab_mergeFeedEnabled: false, // experimental
+  };
+
+export const DEFAULT_THREAD_VIEW_PREFS: ThreadViewPreferences = {
+  sort: "hotness",
+  prioritizeFollowedUsers: true,
+  lab_treeViewEnabled: false,
+};
+
+export const DEFAULT_LOGGED_OUT_PREFERENCES: UsePreferencesQueryResponse = {
+  birthDate: new Date("2022-11-17"), // TODO(pwi)
+  moderationPrefs: {
+    adultContentEnabled: false,
+    labels: DEFAULT_LOGGED_OUT_LABEL_PREFERENCES,
+    labelers: [],
+    mutedWords: [],
+    hiddenPosts: [],
+  },
+  feedViewPrefs: DEFAULT_HOME_FEED_PREFS,
+  threadViewPrefs: DEFAULT_THREAD_VIEW_PREFS,
+  userAge: 13, // TODO(pwi)
+  interests: { tags: [] },
+  savedFeeds: [],
+  bskyAppState: {
+    queuedNudges: [],
+    activeProgressGuide: undefined,
+    nuxs: [],
+  },
+  postInteractionSettings: {
+    threadgateAllowRules: undefined,
+    postgateEmbeddingRules: [],
+  },
+};
