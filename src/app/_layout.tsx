606e1ff7c9a87a3a0defb0d91b2eaa1bbea4c2dd
roth-dev <roth.dev.ops@gmail.com>
2025-02-27 23:24:12 +0700

    feat: refactor app layout to improve session management and structure

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,34 @@
 import "./globals.css";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { QueryProvider } from "@/lib/react-query";
+import {
+  SessionAccount,
+  Provider as SessionProvider,
+  useSession,
+  useSessionApi,
+} from "@/state/session";
+import { Provider as ModerationProvider } from "@/state/prefs/moderation-opts";
+import { Provider as LabelDefsProvider } from "@/state/prefs/label-defs";
+import { Provider as PrefsStateProvider } from "@/state/prefs";
+import { readLastActiveAccount } from "@/state/session/util";
+import { tryFetchGates, Provider as StatsigProvider } from "@/statsig/statsig";
+import { init as initPersistedState } from "@/state/persisted";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { QueryProvider } from "@/lib/react-query";
-import { Provider as SessionProvider, useSession } from "@/state/session";
+import { router, Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
-function RootLayout() {
+function Root() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
@@ -36,30 +45,66 @@ function RootLayout() {
   }
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="auth/welcome" options={{ animation: "none" }} />
-        <Stack.Screen
-          name="auth/login"
-          options={{
-            presentation: "modal",
-          }}
-        />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <Stack screenOptions={{ headerShown: false }} />
     </ThemeProvider>
   );
 }
 
-export default function App() {
+function InnerApp() {
   const { currentAccount } = useSession();
+
+  const { resumeSession } = useSessionApi();
+
+  // init
+  useEffect(() => {
+    async function onLaunch(account?: SessionAccount) {
+      try {
+        if (account) {
+          await resumeSession(account);
+        } else {
+          await tryFetchGates(undefined, "prefer-fresh-gates");
+        }
+      } catch (e) {
+        // logger.error(`session: resume failed`, {message: e})
+      } finally {
+        // (true)
+      }
+    }
+    const account = readLastActiveAccount();
+    onLaunch(account);
+  }, [resumeSession]);
+
+  return (
+    <QueryProvider currentDid={currentAccount?.did}>
+      <LabelDefsProvider>
+        <ModerationProvider>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <Root />
+          </GestureHandlerRootView>
+        </ModerationProvider>
+      </LabelDefsProvider>
+    </QueryProvider>
+  );
+}
+
+export default function App() {
+  const [isReady, setReady] = useState(false);
+
+  useEffect(() => {
+    Promise.all([initPersistedState()]).then(() => setReady(true));
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
 
   return (
     <SessionProvider>
-      <QueryProvider currentDid={currentAccount?.did}>
-        <RootLayout />
-      </QueryProvider>
+      <PrefsStateProvider>
+        <StatsigProvider>
+          <InnerApp />
+        </StatsigProvider>
+      </PrefsStateProvider>
     </SessionProvider>
   );
 }
