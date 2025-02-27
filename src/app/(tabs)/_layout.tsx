fe59897a86876769b6d0975cc16256789dd393fe
roth-dev <roth.dev.ops@gmail.com>
2025-02-27 23:24:58 +0700

    feat: update tab layout with new screen names and icons for improved navigation

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -7,6 +7,8 @@ import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { Ionicons } from "@expo/vector-icons";
+import { isIOS } from "@/platform/detection";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -15,33 +17,64 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
+        // headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: "absolute",
+            position: isIOS ? "absolute" : undefined,
           },
           default: {},
         }),
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
-          title: "Home",
+          title: "Feed",
+          tabBarShowLabel: false,
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="house.fill" color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="search"
         options={{
-          title: "Explore",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          tabBarLabel: "Search",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="search" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="post"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="add" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="video-feed"
+        options={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="videocam" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person" size={size} color={color} />
           ),
         }}
       />
