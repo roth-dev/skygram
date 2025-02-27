b01bc75e3362aa3bd4d495b1f9f51ec3d11f2756
roth-dev <roth.dev.ops@gmail.com>
2025-02-27 23:27:02 +0700

    feat: enhance Button component with improved styling and structure

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
-import {
-  TouchableOpacity,
-  TouchableOpacityProps,
-  TextProps,
-} from "react-native";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { Slot } from "expo-router";
 import { IconSymbol, IconSymbolProps } from "./IconSymbol";
-import { Text } from "./text";
+import { Text, TextProps } from "./text";
 
 const buttonVariants = cva(
-  "flex-row inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "flex-row inline-flex bg-white dark:bg-neutral-950 items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -22,12 +18,12 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background web:shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-gray-300 text-secondary-foreground web:shadow-sm hover:bg-secondary/80",
+          "bg-neutral-200 text-secondary-foreground web:shadow-sm hover:bg-secondary/80 dark:border dark:border-white",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2",
+        default: "h-9 px-4 web:py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
@@ -56,18 +52,16 @@ const Button = ({
   const Comp = asChild ? Slot : TouchableOpacity;
 
   return (
-    <Comp
-      className={cn(buttonVariants({ variant, size, className }))}
+    <TouchableOpacity
       // ref={ref}
       {...props}
+      className={cn(buttonVariants({ variant, size, className }))}
     />
   );
 };
 
 Button.Text = function (props: React.PropsWithChildren<TextProps>) {
-  return (
-    <Text {...props} className={cn("text-white text-md", props.className)} />
-  );
+  return <Text {...props} />;
 };
 
 Button.Icon = function (props: React.PropsWithChildren<IconSymbolProps>) {
