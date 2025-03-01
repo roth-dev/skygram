ccd9b37e0235276af774f0ece18e96777503fb19
roth-dev <roth.dev.ops@gmail.com>
2025-03-01 17:44:20 +0700

    feat: update babel config for web export

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,34 @@
 module.exports = function (api) {
   api.cache(true);
+  // const isTestEnv = process.env.NODE_ENV === 'test';
+
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      [
+        "babel-preset-expo",
+        { 
+          jsxImportSource: "nativewind",
+          lazyImports: true,
+          // native: {
+          //   disableImportExportTransform: !isTestEnv,
+          // },
+        }
+      ],
       "nativewind/babel"
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./src",
+            lib: "./src/lib",
+            platform: "./src/platform",
+            state: "./src/state",
+            crypto: "./src/platform/crypto.ts",
+          },
+        },
+      ]
     ],
   };
 };
