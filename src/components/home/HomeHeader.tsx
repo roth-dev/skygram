6c3c079fa93e4b45ad73eed4d4f61e532a1da93a
roth-dev <roth.dev.ops@gmail.com>
2025-03-02 20:08:25 +0700

    feat: create HomeHeaderLayout component as a wrapper for HomeHeaderLayoutMobile

---
+++ src/components/home/HomeHeader.tsx
@@ -0,0 +1,136 @@
+import React, { useCallback, useState } from "react";
+import { FeedSourceInfo } from "@/state/queries/feed";
+import { useSession } from "@/state/session";
+import { RenderTabBarFnProps } from "../pager/Pager";
+import { HomeHeaderLayout } from "./HomeHeaderLayout";
+import * as DropdownMenu from "zeego/dropdown-menu";
+import { Button } from "../ui/button";
+import { HStack, View } from "../ui";
+import Logo from "../Logo";
+import { Ionicons } from "@expo/vector-icons";
+import Spacer from "../ui/spacer";
+
+function HeaderDropdown({
+  pinnedNames,
+  onChange,
+}: {
+  pinnedNames: string[];
+  onChange: (index: number) => void;
+}) {
+  const [selected, setSelected] = useState("Discover");
+
+  const onSelectPinnedName = useCallback(
+    (index: number) => {
+      setSelected(pinnedNames[index]);
+      onChange(index);
+    },
+    [onChange, pinnedNames]
+  );
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Button variant="ghost" className="items-center">
+          <Button.Text size="2xl" font="semiBold">
+            {selected}
+          </Button.Text>
+          <Button.Icon name="chevron.down" size={14} />
+        </Button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        {pinnedNames.map((name, index) => {
+          return (
+            <DropdownMenu.Item
+              onSelect={() => onSelectPinnedName(index)}
+              key={name}
+            >
+              <DropdownMenu.ItemTitle style={{ fontSize: 12 }}>
+                {name}
+              </DropdownMenu.ItemTitle>
+              {name === selected && (
+                <DropdownMenu.ItemIcon
+                  ios={{
+                    name: "checkmark",
+                  }}
+                  androidIconName="check"
+                />
+              )}
+            </DropdownMenu.Item>
+          );
+        })}
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+}
+
+export function HomeHeader(
+  props: RenderTabBarFnProps & {
+    testID?: string;
+    onPressSelected: () => void;
+    feeds: FeedSourceInfo[];
+  }
+) {
+  const { feeds } = props;
+  const { hasSession } = useSession();
+
+  const hasPinnedCustom = React.useMemo<boolean>(() => {
+    if (!hasSession) return false;
+    return feeds.some((tab) => {
+      const isFollowing = tab.uri === "following";
+      return !isFollowing;
+    });
+  }, [feeds, hasSession]);
+
+  const items = React.useMemo(() => {
+    const pinnedNames = feeds.map((f) => f.displayName);
+    if (!hasPinnedCustom) {
+      return pinnedNames.concat("Feeds ✨");
+    }
+    return pinnedNames;
+  }, [hasPinnedCustom, feeds]);
+
+  const onPressFeedsLink = React.useCallback(
+    () => {
+      // navigation.navigate('Feeds')
+    },
+    [
+      // navigation
+    ]
+  );
+
+  const onSelect = React.useCallback(
+    (index: number) => {
+      if (!hasPinnedCustom && index === items.length - 1) {
+        onPressFeedsLink();
+      } else if (props.onSelect) {
+        props.onSelect(index);
+      }
+    },
+    [items.length, onPressFeedsLink, props, hasPinnedCustom]
+  );
+
+  return (
+    <HomeHeaderLayout tabBarAnchor={props.tabBarAnchor}>
+      <HStack>
+        <View className="w-[120px]">
+          <HeaderDropdown onChange={onSelect} pinnedNames={items} />
+        </View>
+        <Spacer />
+        <View className="w-[120px] items-center">
+          <Logo />
+        </View>
+        <Spacer />
+        <View className="w-[120px] items-end">
+          <HStack className="right-4">
+            <Button variant="ghost" size="icon">
+              <Ionicons name="chatbubble-ellipses-outline" size={22} />
+            </Button>
+            <Button variant="ghost" size="icon">
+              <Ionicons name="notifications-outline" size={22} />
+            </Button>
+          </HStack>
+        </View>
+      </HStack>
+    </HomeHeaderLayout>
+  );
+}
