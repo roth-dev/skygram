ffbe51d088483a00295d5a49966b1b4765268659
roth-dev <roth.dev.ops@gmail.com>
2025-03-03 22:23:04 +0700

    feat: implement HomeScreen component with feed integration and pager functionality

---
+++ src/app/(tabs)/home.tsx
@@ -0,0 +1,117 @@
+import Feed from "@/components/feed";
+import { HomeHeader } from "@/components/home/HomeHeader";
+import { Pager, PagerRef, RenderTabBarFnProps } from "@/components/pager/Pager";
+import { Text } from "@/components/ui";
+import { emitSoftReset } from "@/state/events";
+import { usePinnedFeedsInfos } from "@/state/queries/feed";
+import { FeedParams } from "@/state/queries/post-feed";
+import { usePreferencesQuery } from "@/state/queries/prefs";
+import { useSetMinimalShellMode } from "@/state/shell/minimal-mode";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { ActivityIndicator, SafeAreaView } from "react-native";
+
+export default function HomeScreen() {
+  const { data: preferences } = usePreferencesQuery();
+  const { data: pinnedFeedInfos, isLoading: isPinnedFeedsLoading } =
+    usePinnedFeedsInfos();
+  const setMinimalShellMode = useSetMinimalShellMode();
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const pagerRef = useRef<PagerRef>(null);
+
+  const onPageSelected = useCallback((index: number) => {
+    setSelectedIndex(index);
+  }, []);
+
+  const onPageScrollStateChanged = useCallback(
+    (state: "idle" | "dragging" | "settling") => {
+      "worklet";
+      if (state === "dragging") {
+        setMinimalShellMode(false);
+      }
+    },
+    [setMinimalShellMode]
+  );
+
+  const onPressSelected = useCallback(() => {
+    emitSoftReset();
+  }, []);
+
+  const allFeeds = useMemo(
+    () => (pinnedFeedInfos ?? []).map((f) => f.feedDescriptor),
+    [pinnedFeedInfos]
+  );
+
+  const homeFeedParams = useMemo<FeedParams>(() => {
+    return {
+      mergeFeedEnabled: Boolean(
+        preferences?.feedViewPrefs.lab_mergeFeedEnabled
+      ),
+      mergeFeedSources: preferences?.feedViewPrefs.lab_mergeFeedEnabled
+        ? preferences.savedFeeds
+            .filter((f) => f.type === "feed" || f.type === "list")
+            .map((f) => f.value)
+        : [],
+    };
+  }, [preferences]);
+
+  const renderTabBar = useCallback(
+    (props: RenderTabBarFnProps) => {
+      return (
+        <HomeHeader
+          key="FEEDS_TAB_BAR"
+          {...props}
+          testID="homeScreenFeedTabs"
+          onPressSelected={onPressSelected}
+          feeds={pinnedFeedInfos ?? []}
+          onSelect={(index) => {
+            pagerRef.current?.setPage(index);
+            pagerRef.current?.setPageWithoutAnimated(index);
+            setSelectedIndex(index);
+          }}
+        />
+      );
+    },
+    [onPressSelected, pinnedFeedInfos]
+  );
+
+  if (!preferences && !pinnedFeedInfos && isPinnedFeedsLoading) {
+    return <ActivityIndicator size="large" />;
+  }
+
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <Pager
+        key={allFeeds?.join(",")}
+        ref={pagerRef}
+        testID="homeScreen"
+        swipeEnabled={false}
+        initialPage={selectedIndex}
+        onPageSelected={onPageSelected}
+        onPageScrollStateChanged={onPageScrollStateChanged}
+        renderTabBar={renderTabBar}
+      >
+        {pinnedFeedInfos &&
+          pinnedFeedInfos.length &&
+          pinnedFeedInfos.map((feedInfo, index) => {
+            const feed = feedInfo.feedDescriptor;
+            const isFollowing = feed === "following";
+            const savedFeedConfig = feedInfo.savedFeed;
+            return (
+              <Feed
+                key={feed}
+                testID="customFeedPage"
+                isPageFocused={selectedIndex === index}
+                feed={feedInfo.feedDescriptor}
+                feedParams={homeFeedParams}
+                renderEmptyState={() => <Text>No feed</Text>}
+                renderEndOfFeed={() => <Text>End of feed</Text>}
+                feedInfo={feedInfo}
+                savedFeedConfig={isFollowing ? undefined : savedFeedConfig}
+                isPageAdjacent={Math.abs(selectedIndex - index) === 1}
+              />
+            );
+          })}
+      </Pager>
+    </SafeAreaView>
+  );
+}
