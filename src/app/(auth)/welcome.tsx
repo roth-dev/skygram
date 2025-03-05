7d0469c13f372e9ade40e3defaf2b669436b3e91
roth-dev <roth.dev.ops@gmail.com>
2025-03-05 21:38:39 +0700

    feat: refactor Welcome component layout and styling

--- src/app/(auth)/welcome.tsx
+++ src/app/(auth)/welcome.tsx
@@ -1,41 +1,49 @@
+import Logo from "@/components/Logo";
+import { HStack, Text, View } from "@/components/ui";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "expo-router";
-import { SafeAreaView, Text, View } from "react-native";
 
 export default function Welcome() {
   const router = useRouter();
 
   return (
-    <SafeAreaView className="flex-1 m-3 items-center justify-center bg-muted p-6 md:p-10">
-      <View className="w-full gap-3">
-        <Text className="font-semibold text-2xl text-center">Welcome back</Text>
-        <Button
-          onPress={() => {
-            router.push({
-              pathname: "/(auth)/login",
-              params: {
-                type: "singup",
-              },
-            });
-          }}
-        >
-          <Button.Text>Create an account</Button.Text>
-        </Button>
-        <Button
-          onPress={() => {
-            router.push({
-              pathname: "/(auth)/login",
-              params: {
-                type: "signin",
-              },
-            });
-          }}
-          variant="secondary"
-          className="bg-neutral-200"
-        >
-          <Button.Text className="text-gray-800">Sing in</Button.Text>
-        </Button>
-      </View>
-    </SafeAreaView>
+    <View className="flex-1 p-4 justify-center gap-3">
+      <HStack className="justify-center">
+        <Logo width={120} height={120} />
+      </HStack>
+      <Text font="bold" className="font-semibold text-2xl text-center">
+        Welcome back
+      </Text>
+      <Button
+        onPress={() => {
+          router.push({
+            pathname: "/login",
+            params: {
+              type: "singup",
+            },
+          });
+        }}
+      >
+        <Button.Text font="semiBold" className="text-white">
+          Create an account
+        </Button.Text>
+      </Button>
+      <Button
+        onPress={() => {
+          router.push({
+            pathname: "/login",
+            params: {
+              type: "signin",
+            },
+          });
+        }}
+        variant="secondary"
+        className="bg-neutral-200"
+      >
+        <Button.Text font="semiBold" className="text-gray-800">
+          Sing in
+        </Button.Text>
+      </Button>
+    </View>
   );
 }
