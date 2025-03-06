53ef1ada4c219b93d9002c2dafffcc1747ae91ee
roth-dev <roth.dev.ops@gmail.com>
2025-03-06 22:29:00 +0700

    feat: enhance user profile header

--- src/components/profile/user-profile-header.tsx
+++ src/components/profile/user-profile-header.tsx
@@ -1,62 +1,83 @@
 import { HStack, Text, View, VStack } from "@/components/ui";
 import { Button } from "@/components/ui/button";
-import { Image } from "expo-image";
 import { RichText } from "../ui/rich-text";
 
 import useRichText from "@/hooks/useRichText";
 import { router } from "expo-router";
 import { UserProfileProps } from "./type";
+import { AppBskyActorDefs } from "@atproto/api";
+import UserAvatar from "../UserAvatar";
+import { useMemo } from "react";
+import { useSession } from "@/state/session";
+import { cn } from "@/lib/utils";
+import Separetor from "../ui/separator";
+import { formatNumberToKOrM } from "@/lib/numbers";
 
 export default function UserProfileHeader({
   profile,
   isOwner,
 }: UserProfileProps) {
+  const { currentAccount } = useSession();
   const [richText] = useRichText(profile?.description ?? "");
 
+  const knownFollowers = useMemo(() => {
+    return profile.viewer?.knownFollowers?.followers.slice(0, 3) ?? [];
+  }, [profile]);
+
+  const isMe = useMemo(() => {
+    return profile.did === currentAccount?.did;
+  }, [profile]);
   return (
-    <View className="gap-2 ">
-      <VStack className="p-3">
+    <View className="gap-2">
+      <VStack className="p-3 pb-0">
         <HStack className="items-center">
-          <View className="h-20 w-20 rounded-full border border-gray-200 overflow-hidden bg-gray-300 ">
-            <Image
-              source={{ uri: profile.avatar }}
-              contentFit="contain"
-              style={{ width: "100%", height: "100%" }}
-            />
-          </View>
+          <UserAvatar className="h-20 w-20" avatar={profile.avatar} />
+
           <Button variant="ghost">
-            <VStack className="items-center">
-              <Button.Text>Post</Button.Text>
-              <Button.Text font="semiBold">{profile.postsCount}</Button.Text>
-            </VStack>
-          </Button>
-          <Button variant="ghost">
-            <VStack className="items-center">
-              <Button.Text>Followers</Button.Text>
+            <VStack className="items-center gap-0">
               <Button.Text font="semiBold">
-                {profile.followersCount}
+                {formatNumberToKOrM(profile.postsCount ?? 0)}
               </Button.Text>
+              <Button.Text>Post</Button.Text>
             </VStack>
           </Button>
           <Button variant="ghost">
-            <VStack className="items-center">
+            <VStack className="items-center gap-0">
+              <Button.Text font="semiBold">
+                {formatNumberToKOrM(profile.followersCount ?? 0)}
+              </Button.Text>
+              <Button.Text>Follower</Button.Text>
+            </VStack>
+          </Button>
+          <Button variant="ghost">
+            <VStack className="items-center gap-0">
+              <Button.Text font="semiBold">
+                {formatNumberToKOrM(profile.followsCount ?? 0)}
+              </Button.Text>
               <Button.Text>Following</Button.Text>
-              <Button.Text font="semiBold">{profile.followsCount}</Button.Text>
             </VStack>
           </Button>
         </HStack>
         <VStack>
-          <Text font="extrabold">{profile.displayName}</Text>
-          <Text size="sm">@{profile.handle}</Text>
-          {!!richText && (
-            <RichText value={richText} enableTags size="sm" numberOfLines={4} />
-          )}
+          <Text font="extrabold" size="xl">
+            {profile.displayName}
+          </Text>
+          <Text>@{profile.handle}</Text>
+          {!!richText && <RichText value={richText} enableTags />}
         </VStack>
+
+        {!isMe && knownFollowers.length > 0 && (
+          <KnownFollower
+            count={profile.viewer?.knownFollowers?.count ?? 0}
+            profiles={knownFollowers}
+          />
+        )}
+
         <HStack className="py-3">
           {isOwner ? (
             <Button
               onPress={() => {
-                router.push("/(public)/user-profile/edit");
+                router.push("/user-profile/edit");
               }}
               variant="secondary"
               className="flex-1"
@@ -75,6 +96,57 @@ export default function UserProfileHeader({
           )}
         </HStack>
       </VStack>
+      <Separetor />
     </View>
+  );
+}
+
+function KnownFollower({
+  profiles,
+  count,
+}: {
+  count: number;
+  profiles: AppBskyActorDefs.ProfileViewBasic[];
+}) {
+  const followersName = useMemo(() => {
+    return profiles
+      .map((user) => user.displayName)
+      .filter(Boolean)
+      .join(", ");
+  }, [profiles]);
+
+  const restFollowerCount = useMemo(() => {
+    return count - profiles.length;
+  }, [count, profiles]);
+
+  return (
+    <HStack className="gap-0 items-center ">
+      {profiles.map((user, index) => {
+        return (
+          <UserAvatar
+            style={{
+              right: index === 0 ? 0 : index * 10,
+            }}
+            className={"border border-neutral-100"}
+            key={user.did}
+            avatar={user.avatar}
+          />
+        );
+      })}
+      <HStack
+        className={cn(
+          "gap-1",
+          profiles.length === 1 ? "left-2" : "left-[-12px]"
+        )}
+      >
+        <Text size="sm" numberOfLines={2}>
+          Followed by{" "}
+          <Text font="semiBold" size="sm" numberOfLines={1}>
+            {followersName}
+          </Text>
+          {!!restFollowerCount && ` & ${restFollowerCount} others`}
+        </Text>
+      </HStack>
+    </HStack>
   );
 }
