049f44a8561c7e170d94f0cd6a950644ddd1bc1d
roth-dev <roth.dev.ops@gmail.com>
2025-03-30 23:14:33 +0700

    feat: add shouldHeaderReady prop to UserProfileProps type

--- src/components/profile/type.ts
+++ src/components/profile/type.ts
@@ -2,5 +2,6 @@ import { AppBskyActorDefs } from "@atproto/api";
 
 export type UserProfileProps = {
   isOwner?: boolean;
+  shouldHeaderReady?: boolean;
   profile: AppBskyActorDefs.ProfileViewDetailed;
 };
