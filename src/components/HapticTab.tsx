9a3888a79978b5b1264d35ff634dd1e29bdfc6e3
roth-dev <roth.dev.ops@gmail.com>
2025-04-05 23:58:32 +0700

    feat: add double tap functionality with haptic feedback to HapticTab component

--- src/components/HapticTab.tsx
+++ src/components/HapticTab.tsx
@@ -1,18 +1,42 @@
 import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
 import { PlatformPressable } from "@react-navigation/elements";
 import * as Haptics from "expo-haptics";
+import { useCallback, useRef } from "react";
 
-export function HapticTab(props: BottomTabBarButtonProps) {
-  return (
-    <PlatformPressable
-      {...props}
-      onPressIn={(ev) => {
-        if (process.env.EXPO_OS === "ios") {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+interface HapticTabProps extends BottomTabBarButtonProps {
+  onDoubleTap?: () => void;
+}
+
+export function HapticTab(props: HapticTabProps) {
+  const lastTap = useRef<number>(0);
+
+  const handlePress = useCallback(
+    (ev: any) => {
+      const now = Date.now();
+      const DOUBLE_PRESS_DELAY = 300;
+
+      if (props.onPressIn) {
+        props.onPressIn(ev);
+      }
+
+      if (process.env.EXPO_OS === "ios") {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+      }
+
+      if (now - lastTap.current < DOUBLE_PRESS_DELAY) {
+        // Double tap detected
+        if (props.onDoubleTap) {
+          props.onDoubleTap();
+          if (process.env.EXPO_OS === "ios") {
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+          }
         }
-        props.onPressIn?.(ev);
-      }}
-    />
+      }
+
+      lastTap.current = now;
+    },
+    [props.onPressIn, props.onDoubleTap]
   );
+
+  return <PlatformPressable {...props} onPressIn={handlePress} />;
 }
