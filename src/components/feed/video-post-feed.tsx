a414197ada077145ef96b27881ba623e6e991fe6
roth-dev <roth.dev.ops@gmail.com>
2025-04-10 23:18:24 +0700

    feat: refactor VideoGrid to use VideoGridItem component for improved readability and maintainability

--- src/components/feed/video-post-feed.tsx
+++ src/components/feed/video-post-feed.tsx
@@ -1,14 +1,15 @@
 import { HStack, Text, View, VStack } from "../ui";
 import { FeedPostSliceItem } from "@/state/queries/post-feed";
-import { Pressable } from "react-native";
+import { Pressable, StyleProp, ViewStyle } from "react-native";
 import { Image } from "expo-image";
-import { useCallback } from "react";
+import { memo, useCallback, useMemo } from "react";
 import { formatNumberToKOrM } from "@/lib/numbers";
 import { Colors } from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import Animated from "react-native-reanimated";
-import { Link } from "expo-router";
+import { Link, LinkProps } from "expo-router";
 import { VideoFeedSourceContext } from "../video/type";
+import { cn } from "@/lib/utils";
 
 interface Props {
   items: FeedPostSliceItem[];
@@ -29,62 +30,73 @@ interface VideoGridProps {
 const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 function VideoGrid({ items }: VideoGridProps) {
-  const renderItem = useCallback((item: FeedPostSliceItem, index: number) => {
-    const thumbnail = item.post.embed?.thumbnail as string;
-    const sharedID = `shareID=${index}-${thumbnail}`;
-    return (
-      <VStack
-        key={item._reactKey}
-        className="w-1/3 h-[190px] mb-[0.9px] bg-neutral-50"
-      >
-        <Link
-          href={{
-            pathname: "/video-player",
-            params: {
-              type: "author",
-              thumbnail: thumbnail,
-              initialPostUri: item.uri,
-              did: item.post.author.did,
-              filter: "posts_with_video",
-            },
-          }}
-          asChild
-        >
-          <Pressable
-            style={{ flex: 1 }}
-            onPressIn={async () => {
-              await Image.prefetch(thumbnail);
-            }}
-          >
-            <AnimatedImage
-              sharedTransitionTag={sharedID}
-              source={{ uri: thumbnail }}
-              style={{ flex: 1 }}
-              transition={800}
-            />
-          </Pressable>
-        </Link>
-
-        <View className="absolute bottom-2 left-2 bg-transparent">
-          <HStack className="bg-transparent gap-1">
-            <Ionicons
-              size={16}
-              name="heart"
-              className="shadow-black shadow"
-              color={Colors.light.background}
-            />
-            <Text
-              font="semiBold"
-              size="sm"
-              className="text-white shadow shadow-black"
-            >
-              {formatNumberToKOrM(item.post.likeCount ?? 0)}
-            </Text>
-          </HStack>
-        </View>
-      </VStack>
-    );
+  const renderItem = useCallback((item: FeedPostSliceItem) => {
+    return <VideoGridItem {...item} key={item._reactKey} />;
   }, []);
 
   return <HStack className="gap-[0.9px]">{items.map(renderItem)}</HStack>;
 }
+
+interface VideoGridItemProps extends FeedPostSliceItem {
+  className?: string;
+  style?: StyleProp<ViewStyle>;
+}
+export const VideoGridItem = memo(function Impl({
+  className,
+  style,
+  ...item
+}: VideoGridItemProps) {
+  const thumbnail = item.post.embed?.thumbnail as string;
+  const sharedID = `trending-video-${thumbnail}`;
+
+  const linkParams = useMemo(
+    () => ({
+      pathname: "/video-player",
+      params: {
+        sharedID,
+        type: "author",
+        thumbnail,
+        initialPostUri: item.uri,
+        did: item.post.author.did,
+        filter: "posts_with_video",
+      },
+    }),
+    [sharedID, thumbnail, item.uri, item.post.author.did]
+  );
+  return (
+    <VStack
+      style={style}
+      className={cn("w-1/3 h-[190px] mb-[0.9px] bg-neutral-50", className)}
+    >
+      <Link href={linkParams as LinkProps["href"]} asChild>
+        <Pressable style={{ flex: 1 }}>
+          <AnimatedImage
+            sharedTransitionTag={sharedID}
+            source={{ uri: thumbnail }}
+            contentFit="cover"
+            style={{ flex: 1 }}
+            transition={800}
+          />
+        </Pressable>
+      </Link>
+
+      <View className="absolute bottom-2 left-2 bg-transparent">
+        <HStack className="bg-transparent gap-1">
+          <Ionicons
+            size={16}
+            name="heart"
+            className="shadow-black shadow"
+            color={Colors.light.background}
+          />
+          <Text
+            font="semiBold"
+            size="sm"
+            className="text-white shadow shadow-black"
+          >
+            {formatNumberToKOrM(item.post.likeCount ?? 0)}
+          </Text>
+        </HStack>
+      </View>
+    </VStack>
+  );
+});
