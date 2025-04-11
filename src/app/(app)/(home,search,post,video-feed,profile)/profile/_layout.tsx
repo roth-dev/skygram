b7a7e57614d03fa90c8fe4cd488db18f1a84a110
roth-dev <roth.dev.ops@gmail.com>
2025-04-11 23:40:14 +0700

    feat: enhance Profile component with logout functionality and improved layout

--- src/app/(app)/(home,search,post,video-feed,profile)/profile/_layout.tsx
+++ src/app/(app)/(home,search,post,video-feed,profile)/profile/_layout.tsx
@@ -1,13 +1,18 @@
-import { ActivityIndicator } from "react-native";
-import { useSession } from "@/state/session";
+import { ActivityIndicator, SafeAreaView } from "react-native";
+import { useAgent, useSession } from "@/state/session";
 import { useProfileQuery } from "@/state/queries/profile";
 import { View } from "@/components/ui";
 import UserProfile from "@/components/profile";
 import Layout from "@/components/Layout";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 
 export default function Profile() {
   const { currentAccount } = useSession();
-  const { data, isFetching } = useProfileQuery({ did: currentAccount?.did });
+  const { logout } = useAgent();
+  const { data, isFetching, isPlaceholderData } = useProfileQuery({
+    did: currentAccount?.did,
+  });
 
   if (!data || isFetching) {
     return (
@@ -18,8 +23,24 @@ export default function Profile() {
   }
 
   return (
-    <Layout.Tab headerShown>
-      <UserProfile isOwner profile={data} />
+    <Layout.Tab
+      headerShown={true}
+      headerRight={() => (
+        <Ionicons
+          name="settings-outline"
+          size={24}
+          onPress={() => logout()}
+          className="text-foreground"
+        />
+      )}
+    >
+      <SafeAreaView className="flex-1 bg-white dark:bg-black">
+        <UserProfile
+          shouldHeaderReady={!isPlaceholderData}
+          isOwner
+          profile={data}
+        />
+      </SafeAreaView>
     </Layout.Tab>
   );
 }
