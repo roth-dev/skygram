26fa707c634a50a17543fd0c9912a71fb8b552ce
roth-dev <roth.dev.ops@gmail.com>
2025-05-31 23:21:46 +0700

    add accessibility screen

---
+++ src/app/(app)/(modal)/accessibility.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import { Text, View } from "@/components/ui";
+
+export default function AccessibilityScreen() {
+  return (
+    <Layout.Screen>
+      <View>
+        <Text>Accessibility screen</Text>
+      </View>
+    </Layout.Screen>
+  );
+}
