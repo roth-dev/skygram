eda03485257fc6c6aa5409bd9bece07614c62111
roth-dev <roth.dev.ops@gmail.com>
2025-06-04 10:26:32 +0700

    feat: enhance ExpoIcon component with size prop support

--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -2,18 +2,37 @@ import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { useThem } from "@/context/theme-provider";
 
+const IconSize = {
+  xs: 12,
+  sm: 14,
+  md: 16,
+  base: 18,
+  lg: 22,
+  xl: 24,
+  "2xl": 26,
+  "3xl": 32,
+};
+
 export type ExpoIconType = keyof typeof Ionicons.glyphMap;
+
+export type IoniconsProps = React.ComponentProps<typeof Ionicons>;
+export interface ExpoIconProps extends Omit<IoniconsProps, "size"> {
+  size?: keyof typeof IconSize;
+}
 
 const ExpoIcon = React.forwardRef<
   React.ComponentRef<typeof Ionicons>,
-  React.ComponentProps<typeof Ionicons>
->((props, ref) => {
+  ExpoIconProps
+>(({ size = "base", ...props }, ref) => {
   const { currentScheme } = useThem();
   return (
     <Ionicons
       ref={ref}
       {...props}
-      color={props.color ?? currentScheme === "dark" ? "#fff" : "#000"}
+      size={IconSize[size]}
+      color={
+        props.color ? props.color : currentScheme === "dark" ? "#fff" : "#000"
+      }
     />
   );
 });
